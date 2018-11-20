8a80e0f31d3b9af83dfbc6030bc6873cbb4a7b84
Mario Beltrán Alarcón <belco90@gmail.com>
2018-11-21 00:08:35 +0100

    Show simple main bar

--- src/App.js
+++ src/App.js
@@ -1,28 +1,18 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React  from 'react';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import MainBar from './components/MainBar';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+const theme = createMuiTheme({
+  palette: {
+    primary: { main: '#394eff' },
+    secondary: { main: '#666666' },
+  },
+});
+
+const App = () => (
+  <MuiThemeProvider theme={theme}>
+    <MainBar />
+  </MuiThemeProvider>
+);
 
 export default App;
---
+++ src/components/MainBar/MainBar.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import AccountCircle from '@material-ui/icons/AccountCircle';
+
+import styles from './MainBar.module.css';
+
+const MainBar = () => (
+  <div className={styles.root}>
+    <AppBar position="static">
+      <Toolbar>
+        <IconButton className={styles.menuButton} color="inherit" aria-label="Menu">
+          <MenuIcon />
+        </IconButton>
+        <Typography variant="h6" color="inherit" className={styles.grow}>
+          React ❤️ Material Design
+        </Typography>
+        <Button color="inherit"><AccountCircle /></Button>
+      </Toolbar>
+    </AppBar>
+  </div>
+);
+
+export default MainBar;
